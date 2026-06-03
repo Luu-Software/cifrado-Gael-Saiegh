7fca3e3fbaf1a51079e598709811dd26bac625b6
Gael-Saiegh <gaesaiegh@gmail.com>
2026-06-03 15:59:20 -0300

    falta maija

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import { preguntar } from './lib/consola.ts';
 import { cifrar, descifrar } from './lib/funcionesCriptográficas.ts';
 
-let palabra: string = ''; // Preguntar por la palabra a cifrar o descifrar
-let claveTexto: string = ''; // Preguntar por la clave de cifrado (número)
-let accion: string = ''; // Preguntar si se desea cifrar o descifrar (cifrar/descifrar)
+let palabra: string = preguntar('ingrese una palabra a cifrar/descifrar'); // Preguntar por la palabra a cifrar o descifrar
+let claveTexto: string = preguntar('ingrese su clave numerica') ; // Preguntar por la clave de cifrado (número)
+let accion: string = preguntar('indique si quiere cifrar o descifrar '); // Preguntar si se desea cifrar o descifrar (cifrar/descifrar)
 
-let resultado: string = ''; // COMPLETAR
-console.log(resultado);
+let clavenumber= Number(claveTexto)
+let resultado: string = '' // COMPLETAR
+
+ if (accion==='cifrar') {
+ resultado = cifrar(palabra,clavenumber)
+}
+else { 
+ resultado = descifrar(palabra,clavenumber)
+
+     }       
+     console.log(resultado);              
+     
+     
--- src/manija.ts
+++ src/manija.ts
@@ -8,3 +8,15 @@ let tarjetaTexto: string = preguntar('Ingresá el número de tarjeta: ');
 
 let red: string = ''; // COMPLETAR
 console.log(red);
+
+let tarjeta: string= preguntar('indique su tarjeta')
+let tarnumtext: string= preguntar('indique el numero de su tarjeta')
+let tarnum= Number(tarnumtext)
+
+if (accion= 'obtenerprefijo') {
+  if (tarjeta= 'visa') {
+    if (tarnum= '4') {
+        resultado = obtenerprefijo (tarnum)
+    }
+ }
+  }
